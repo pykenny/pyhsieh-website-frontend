a83dd3e6a4775ea9ad475367aeefeec7a5d7896b
Po-Yu Hsieh <pykenny@gmail.com>
2021-02-22 22:36:45 +0800

    Code block toolkit
    Details:
     - Adding interactive code block toolkit

--- src/js/site/blog_post/index.jsx
+++ src/js/site/blog_post/index.jsx
@@ -6,6 +6,7 @@ import htmlReactParser from 'html-react-parser';
 import setIcons from '../../utils/icons';
 import LocalizedTimestamp from '../../common/LocalizedTimestamp';
 import SlideBussinessCard from '../../common/SlideBussinessCard';
+import CodeSnippet from '../../common/CodeSnippet';
 
 setIcons();
 
@@ -14,10 +15,25 @@ setIcons();
   ReactDOM.render(<LocalizedTimestamp {...{ timestamp }} />, elem);
 });
 
+[...document.getElementsByTagName('pre')].forEach((elem) => {
+  const filename = elem.getAttribute('file-name');
+  // const language = elem.getAttribute('language');
+  const codeData = elem.outerHTML;
+  const data = elem.getElementsByTagName('code')[0].textContent;
+  const snippetElem = document.createElement('div');
+  elem.parentNode.replaceChild(snippetElem, elem);
+  ReactDOM.render(
+    <CodeSnippet {...{ filename, data }}>
+      {htmlReactParser(codeData)}
+    </CodeSnippet>,
+    snippetElem,
+  );
+});
+
 // Render process will destroy target element's children, so we
 // clone the text content and turn it into an React element.
 const bannerTarget = document.getElementById('blog-banner-bussiness-card');
-const bannerElement = htmlReactParser(bannerTarget.outerHTML);
+const bannerElement = htmlReactParser(bannerTarget.innerHTML);
 
 ReactDOM.render(
   <SlideBussinessCard>{bannerElement}</SlideBussinessCard>,
